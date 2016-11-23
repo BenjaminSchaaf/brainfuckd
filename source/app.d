f8b355a3163c4e2501117470d38d084b35df5fda
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-11-23 19:31:02 +1100

    Improved IO handling in app.d

--- source/app.d
+++ source/app.d
@@ -17,9 +17,51 @@ void main(string[] args) {
     }
 
     string sourceFile = args[1];
-    auto source = File(sourceFile, "r").byChunk(READ_BLOCK_SIZE).joiner.array.to!(char[]);
-    auto input = stdin.byChunk(READ_BLOCK_SIZE).joiner.map!(to!char);
+    auto source = File(sourceFile, "r").byChunk(READ_BLOCK_SIZE).joiner.array;
+    auto input = lazyRange(() => stdin.byChunk(1)).joiner.map!(to!char);
 
-    auto result = brainfuck(source, input);
-    write(cast(char[])result.array);
+    auto result = brainfuck(cast(char[])source, input);
+    foreach (char chr; result) {
+        write(chr);
+    }
+}
+
+/// A range that lazily wraps another range
+/// The wrapped range is only evaluated once needed
+/// This can be used to avoid blocking IO until needed
+struct LazyRange(R) if (isInputRange!R) {
+    static assert(isInputRange!(LazyRange!(int[])));
+
+    private {
+        R _range;
+        R delegate() lazyFn;
+    }
+
+    this(R delegate() lazyFn) {
+        this.lazyFn = lazyFn;
+    }
+
+    @property bool empty() {
+        return range.empty;
+    }
+
+    @property auto front() {
+        return range.front;
+    }
+
+    void popFront() {
+        range.popFront();
+    }
+
+    private @property ref R range() {
+        if (lazyFn != null) {
+            _range = lazyFn();
+            lazyFn = null;
+        }
+        return _range;
+    }
+}
+
+auto lazyRange(R)(R delegate() lazyFn) {
+    return LazyRange!R(lazyFn);
 }
